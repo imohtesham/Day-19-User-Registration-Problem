47ab26eb1875bfeeb1f7ae6fd66ad0b094f73c99
imohtesham <ibraahimm92@gmail.com>
2022-11-01 19:33:30 +0530

    1 used cases to create valid name

--- UserRegistration.java
+++ UserRegistration.java
@@ -2,3 +2,23 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+public class UserRegistration {
+    static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        UserRegistration user = new UserRegistration();
+        user.firstName();
+
+    }
+    public void firstName() {
+        System.out.println("Enter first Name : ");
+        String firstName = scan.next();
+        Pattern pattren1 = Pattern.compile("^[A-Z][a-z]{2,}");
+        Matcher matcher1 = pattren1.matcher(firstName);
+        if (matcher1.matches()) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+            System.out.println("Enter Name in valid format, first letter should be capital case and remaining in small case");
+        }
+    }
+}
